5dd660d6e61f8139fd0276a644747e48b1d5768f
Yash Digarse <112575988+yashdigarse@users.noreply.github.com>
2024-03-09 22:13:02 -0500

    GA

--- utility.py
+++ utility.py
@@ -5,9 +5,7 @@ import numpy as np
 from numpy.linalg import norm
 import pandas as pd
 import nltk
-import pathlib
-import BeautifulSoup
-import shutil
+
 nltk.download('stopwords')
 
 
@@ -77,18 +75,6 @@ def inject_ga():
 		</script>
     """
 
-    # Insert the script in the head tag of the static template inside your virtual
-    index_path = pathlib.Path(st.__file__).parent / "static" / "index.html"
-    #logging.info(f'editing {index_path}')
-    soup = BeautifulSoup(index_path.read_text(), features="html.parser")
-    if not soup.find(id=GA_ID): 
-        bck_index = index_path.with_suffix('.bck')
-        if bck_index.exists():
-            shutil.copy(bck_index, index_path)  
-        else:
-            shutil.copy(index_path, bck_index)  
-        html = str(soup)
-        new_html = html.replace('<head>', '<head>\n' + GA_JS)
-        index_path.write_text(new_html)
+   
 
 
